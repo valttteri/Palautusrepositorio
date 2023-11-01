56bd606f44e8f77f1c4e035aa57b6f05532b6832
Valtteri Ahola <valtteri.ahola@helsinki.fi>
2023-11-01 13:50:44 +0200

    week 1 exercise 16

---
+++ viikko1/nhl-statistics-1/src/tests/statistics_service_test.py
@@ -0,0 +1,48 @@
+import unittest
+from statistics_service import StatisticsService
+from player import Player
+
+class PlayerReaderStub:
+    def get_players(self):
+        return [
+            Player("Semenko", "EDM", 4, 12),
+            Player("Lemieux", "PIT", 45, 54),
+            Player("Kurri",   "EDM", 37, 53),
+            Player("Yzerman", "DET", 42, 56),
+            Player("Gretzky", "EDM", 35, 89)
+        ]
+
+class TestStatisticsService(unittest.TestCase):
+    def setUp(self):
+        # annetaan StatisticsService-luokan oliolle "stub"-luokan olio
+        self.stats = StatisticsService(
+            PlayerReaderStub()
+        )
+    
+    def test_valid_search(self):
+        player = self.stats.search("Kurri")
+
+        self.assertEqual(player.name, "Kurri")
+        self.assertEqual(player.team, "EDM")
+        self.assertEqual(player.goals, 37)
+        self.assertEqual(player.assists, 53)
+
+    def test_invalid_search(self):
+        self.assertIsNone(self.stats.search("Selanne"))
+    
+    def test_team_returns_correct_players(self):
+        players = self.stats.team("EDM")
+
+        self.assertEqual(len(players), 3)
+        self.assertEqual(players[0].name, "Semenko")
+        self.assertEqual(players[1].name, "Kurri")
+        self.assertEqual(players[2].name, "Gretzky") 
+    
+    def test_top_players_order(self):
+        players_ranked = self.stats.top(5)
+        self.assertEqual(len(players_ranked), 5)
+        self.assertEqual(players_ranked[0].points, 89+35)
+        self.assertEqual(players_ranked[1].points, 45+54)
+        self.assertEqual(players_ranked[2].points, 42+56)
+        self.assertEqual(players_ranked[3].points, 37+53)
+        self.assertEqual(players_ranked[4].points, 4+12)
