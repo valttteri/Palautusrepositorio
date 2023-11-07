ffdaa1fcd28365a3eb961050584f51fab271d94d
Valtteri Ahola <valtteri.ahola@helsinki.fi>
2023-11-07 12:15:28 +0200

    week 2 exercise 10

---
+++ viikko2/nhl-reader/src/index.py
@@ -0,0 +1,21 @@
+import requests
+from player import Player
+
+def main():
+    url = "https://studies.cs.helsinki.fi/nhlstats/2022-23/players"
+    response = requests.get(url).json()
+
+    players = []
+
+    for player_dict in response:
+        if player_dict["nationality"] == "FIN":
+            player = Player(player_dict)
+            players.append(player)
+
+    print("Players from FIN:\n")
+
+    for player in players:
+        print(player)
+
+if __name__ == "__main__":
+    main()
---
+++ viikko2/nhl-reader/src/player.py
@@ -0,0 +1,9 @@
+class Player:
+    def __init__(self, dict):
+        self.name = dict['name']
+        self.team = dict['team']
+        self.goals = dict['goals']
+        self.assists = dict['assists']
+    
+    def __str__(self):
+        return f"{self.name} team {self.team} goals {self.goals} assists {self.assists}"
