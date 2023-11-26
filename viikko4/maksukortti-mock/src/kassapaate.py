a38939b0bbaced62ac9bc8a43886dfc87ce6e27c
Valtteri Ahola <valtteri.ahola@helsinki.fi>
2023-11-26 18:33:49 +0200

    week 4 exercise 2

---
+++ viikko4/maksukortti-mock/src/kassapaate.py
@@ -0,0 +1,16 @@
+HINTA = 5
+
+
+class Kassapaate:
+    def __init__(self):
+        self.myytyja_lounaita = 0
+
+    def lataa(self, kortti, summa):
+        kortti.lataa(summa)
+
+    def osta_lounas(self, kortti):
+        if kortti.saldo >= HINTA:
+            kortti.osta(HINTA)
+            self.myytyja_lounaita = self.myytyja_lounaita + 1
+        else:
+            print("Kortilla ei ole tarpeeksi saldoa")
